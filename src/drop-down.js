d5e751d768455559e760258af6dca8ed7a45225c
luchodeco <72599025+louisdeco@users.noreply.github.com>
2024-10-16 15:17:54 +0400

    refactor(dropdown): modularized code
    Improved the code structure for better modularity and reusability.

--- src/drop-down.js
+++ src/drop-down.js
@@ -1,22 +1,39 @@
 import "./css/style.css";
 
 const dropdown = (function () {
-  const init = () => {
-    const dropdown = document.querySelector(".dropdown");
-    const dropdownToggle = document.querySelector(".dropdown-toggle");
+  const openDropdown = (dropdownMenu, svg) => {
+    dropdownMenu.classList.add("show");
+    svg.classList.add("rotated");
+  };
+
+  const closeDropdown = (dropdownMenu, svg) => {
+    dropdownMenu.classList.remove("show");
+    svg.classList.remove("rotated");
+  };
+
+  const injectSVG = (dropdownToggle) => {
     dropdownToggle.innerHTML +=
       '<svg class="toggle-arrow" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><path d="M7.41,15.41L12,10.83L16.59,15.41L18,14L12,8L6,14L7.41,15.41Z" /></svg>';
-    const dropdownMenu = document.querySelector(".dropdown-menu");
-    const svg = document.querySelector("svg");
+  };
 
-    dropdownToggle.addEventListener("mouseenter", () => {
-      dropdownMenu.classList.add("show");
-      svg.classList.add("rotated");
-    });
-    dropdown.addEventListener("mouseleave", () => {
-      dropdownMenu.classList.remove("show");
-      svg.classList.remove("rotated");
-    });
+  const setupDropdowns = (dropdown) => {
+    const dropdownToggle = dropdown.querySelector(".dropdown-toggle");
+    injectSVG(dropdownToggle);
+
+    const dropdownMenu = dropdown.querySelector(".dropdown-menu");
+    const svg = dropdown.querySelector("svg");
+
+    dropdownToggle.addEventListener("mouseenter", () =>
+      openDropdown(dropdownMenu, svg),
+    );
+    dropdown.addEventListener("mouseleave", () =>
+      closeDropdown(dropdownMenu, svg),
+    );
+  };
+
+  const init = () => {
+    const dropdowns = document.querySelectorAll(".dropdown");
+    dropdowns.forEach(setupDropdowns);
   };
 
   return { init };
